c5bcc0067b8a4a542698014594a589914a17d285
AryanTy20 <devty007@gmail.com>
2022-07-09 18:09:04 +0530

    tweak

--- src/helper/intersectHook.jsx
+++ src/helper/intersectHook.jsx
@@ -15,7 +15,7 @@ export const useIntersectHook = (refItem, option) => {
       option
         ? option
         : {
-            rootMargin: "-100px 0px 0px 0px",
+            rootMargin: "-50px 0px 0px 0px",
             threshold: 0.2,
           }
     );
